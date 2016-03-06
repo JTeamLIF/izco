4ee06c49b26a4a495846cce31bd50e1900814749
Sina <inandsza@gmail.com>
2016-03-06 11:54:34 +0000

    can now dropdown the black div

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 $(function () {
-		$('.imageThumbnail').each(function (item) {
+	$('.imageThumbnail').each(function (item) {
 		$(this).on('click', function() {
 			var thumbID = $(this).attr('id');
 			$('.overSlideShowDiv').css('display', 'none');
 			$('[data-id="' + thumbID + '"]').fadeIn();
 		});
 	});
+
+	$('.glyphicon-triangle-bottom').on('click', function() {
+		var div = $(this).closest('.overSlideShowDiv');
+		var topGlyphIcon = $(this).closest('.overSlideShowDiv').find('.glyphicon-triangle-top');
+		console.log(topGlyphIcon);
+		topGlyphIcon.show();
+		div.css('position', 'absolute');
+		div.animate({ marginTop:'325px' })
+	});
+
+	$('.glyphicon-triangle-top').on('click', function() {
+		var div = $(this).closest('.overSlideShowDiv');
+		$(this).hide();
+		div.animate({ marginTop:'0px' }, 
+			{ 
+				complete: function() {
+					div.css('position', 'relative');
+				}
+			});
+	});
+
 });
 
