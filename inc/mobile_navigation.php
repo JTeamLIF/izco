230e0603c8e92d971300aa0e9233f4d1a11608ca
Alex Cooke <alex.cooke@tektrasystems.com>
2016-03-14 23:07:41 +0000

    nav error mobile

--- inc/mobile_navigation.php
+++ inc/mobile_navigation.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand" href="index,php"></a> </div>
+            <a class="navbar-brand" href="index.php"></a> </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
               <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
